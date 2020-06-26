4b61bad425e424dd5513648791147a6de7d6d648
Dhananjay Choksi <soni.jay008@gmail.com>
2020-06-26 11:56:00 +0530

    Online Shop
    This is a small react js project created for learning and practice React Js.

---
+++ precache-manifest.ed5399028964f1496a9ae8f8d3384dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ba896745ef7fcf9019b2b3b52c8b67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d7ab0daa04e4e6ee15",
+    "url": "/static/css/main.0886e170.chunk.css"
+  },
+  {
+    "revision": "9bb32a1eb1f755d8ccb9",
+    "url": "/static/js/2.9b515d24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b515d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d7ab0daa04e4e6ee15",
+    "url": "/static/js/main.18a51d3b.chunk.js"
+  },
+  {
+    "revision": "eeba65a76f24d8a207cd",
+    "url": "/static/js/runtime-main.867acd4b.js"
+  }
+]);
